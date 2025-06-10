1a02dfc6ce6aa0bc1ec595c2eca1645fc5d10f8e
KrishnanditoLksn <krislaksana123@gmail.com>
2025-06-10 15:26:59 +0700

    delete:unused classification tabs

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ import os
 import streamlit as st
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import MultiLabelBinarizer
 
 from indexer import create_index
-from search import search_index, document_label, split_data_label
+from search import search_index, split_data_label
 
 # Pilih aksi
 option = st.sidebar.selectbox("Pilih Aksi",
-                              ["Index Dokumen", "Cari Dokumen", "Klasifikasi Dokumen", "Informasi Kelas Dokumen"])
+                              ["Index Dokumen", "Cari Dokumen", "Klasifikasi Dokumen"])
 
 # Jalur direktori
 dokumen_dir = "dokumen"
@@ -66,21 +65,14 @@ elif option == "Cari Dokumen":
             #         st.info("Tidak ada ground truth untuk query ini.")
 
 
-elif option == "Informasi Kelas Dokumen":
+elif option == "Klasifikasi Dokumen":
     st.write("Informasi Kelas Dokumen")
-    categories = document_label()
-
-    for i in categories:
-        st.write(i)
-
     df = split_data_label()
     st.header("DATASET ROUTERS")
     st.write(df)
 
     tfidf = TfidfVectorizer(stop_words=stopwords.words('english'), max_features=5000)
     X = tfidf.fit_transform(df['text'])
-    st.header("X FEATURE")
-    st.write(X)
 
     st.write("Encode Label")
     mlb = MultiLabelBinarizer()
@@ -89,15 +81,10 @@ elif option == "Informasi Kelas Dokumen":
     st.write("Split 80% training dan 20% test")
     X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
 
-    st.header("DATA X TRAIN")
-    st.write(X_train)
-
-    st.header("Data Y TRAIN")
-    st.write(Y_train)
-
     model = OneVsRestClassifier(MultinomialNB())
     model.fit(X_train, Y_train)
     y_pred = model.predict(X_test)
 
-    st.header("Matrix Confusion")
-    st.write(confusion_matrix(Y_test, y_pred, labels=mlb.classes_))
+    st.write(y_pred)
+    # st.header("Matrix Confusion")
+    # st.write(confusion_matrix(Y_test, y_pred, labels=mlb.classes_))
